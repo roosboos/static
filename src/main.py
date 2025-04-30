e3fcf55b7105bd8265ae1b1e70ee741a6fd1f3f7
roosboos <john.4@scale.net>
2025-04-30 13:08:00 -0400

    more pages

--- src/main.py
+++ src/main.py
@@ -11,6 +11,10 @@ def main():
     print("Static files copied successfully!")
 
     generate_page("content/index.md", "template.html", "public/index.html")
+    generate_page("content/blog/glorfindel/index.md", "template.html", "public/blog/glorfindel/index.html")
+    generate_page("content/blog/tom/index.md", "template.html", "public/blog/tom/index.html")
+    generate_page("content/blog/majesty/index.md", "template.html", "public/blog/majesty/index.html")
+    generate_page("content/contact/index.md", "template.html", "public/contact/index.html")
 
 
 
